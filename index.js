194d776eb541437cf283a6df9753048847567825
pratyushsahay <pratyush97@gmail.com>
2019-04-12 03:19:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -233,6 +233,6 @@ app.post("/heaviest", function(req, res) {
 });
 
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
